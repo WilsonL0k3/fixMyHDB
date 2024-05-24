5df90e83167ff1832d03563d54b13e28a17e69de
WilsonL0k3 <wilson.loke@outlook.com>
2024-05-24 15:05:48 +0000

    add nav menu

--- scripts.js
+++ scripts.js
@@ -58,3 +58,12 @@ button.addEventListener('click', () => {
 });
 
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
