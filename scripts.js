d140e93274b38db69a0da5f887a69bc8c1d1538b
WilsonL0k3 <wilson.loke@outlook.com>
2024-05-25 03:22:00 +0000

    change background and centralise header

--- scripts.js
+++ scripts.js
@@ -59,11 +59,3 @@ button.addEventListener('click', () => {
 
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-
-    menuIcon.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
